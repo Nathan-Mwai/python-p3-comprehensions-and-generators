9858a17be4e2183488fc8506442447ecc8becc30
Nathan Mwai <nathan.mwai@student.moringaschool.com>
2024-08-28 18:53:40 +0300

    finishes lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_numbers = [n for n in num_list if(n % 2 == 0)]
+    return even_numbers
 
 def make_exclamation(sentence_list):
+    exclaimed_sentences = [sen + "!" for sen in sentence_list]
+    return exclaimed_sentences
     pass
